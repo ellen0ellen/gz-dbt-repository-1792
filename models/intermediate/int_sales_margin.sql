03d47a7855d5bf3713405a6998151657913eaa1d
livia.ellen@lewagon.education <livia.ellen@lewagon.education>
2024-09-11 11:33:59 +0000

    modifying int sales marging using (products_id)

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -10,4 +10,4 @@
       ROUND(s.revenue - s.quantity*p.purchase_price),2) AS margin
   FROM {{ref("stg_raw__sales")}} s
   LEFT JOIN {{ref("stg_raw__product")}} p
-      USING (products_id)
+      USING products_id
